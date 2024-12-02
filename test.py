6019684f39ffee3199fdf59e004d5cc3710bcb67
jiakunli <jacob.jiakunli@gmail.com>
2024-12-01 17:01:48 -0800

    Edited taunt_player function in npc.py, added test.py to test code

---
+++ test.py
@@ -0,0 +1,10 @@
+from Character.player import Player
+from Character.npc import NPC
+
+npc = NPC()
+print(npc.characteristic)
+print(npc.taunt_player())
+print(npc.taunt_player())
+print(npc.taunt_player())
+print(npc.taunt_player())
+print(npc.taunt_player())
